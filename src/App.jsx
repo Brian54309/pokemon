f288803aee70659632a38e68a26e617306bfbbc9
fandok <fandika.okdiba@gmail.com>
2024-07-11 21:16:21 +0700

    adjust stats pokemonm

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import axios from "axios"
-import { poke_url } from './consant'
-import Item from './item'
-const App=()=> {
-  const [pokemon,setPokemon] = useState([])
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { poke_url } from "./consant";
+import Item from "./item";
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-    const response = await fetch(poke_url)
-    const responseData = await response.json()
-    setPokemon(responseData.results)
-    }
+      const response = await fetch(poke_url);
+      const responseData = await response.json();
+      setPokemon(responseData.results);
+    };
 
-    fetchData()  
-  },[])
+    fetchData();
+  }, []);
+
   return (
-    <>
-    <div className='container'>
-        {pokemon.map((item) => {
-          return <Item name={item.name} url={item.url} key={item.name} />;
-        })}
-        </div>
-    </>
-  )
-}
+    <div className="container">
+      {pokemon.map((item) => {
+        return <Item name={item.name} url={item.url} key={item.name} />;
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/item.jsx
+++ src/item.jsx
@@ -1,109 +1,145 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./item.css" 
-import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-const Item=({name,url})=>{
-    const [itemData, setItemData] = useState({
-        name,
-        imageUrl: "",
-        ability:"",
-        height:0,
-        weight:0,
-        type:"",
-        hp:0,
-        attack:0,
-        speed:0,
-        defense:0,
-        specialAttack:0,
-        specialDefense:0
-      });
+import "./item.css";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-      useEffect(() => {
-        const fetchData = async () => {
-          const response = await axios.get(url);
-          setItemData((prev) => ({
-            ...prev,
-            imageUrl: response.data.sprites.front_default,
-            ability:response.data.abilities[0].ability.name,
-            height:response.data.height,
-            weight:response.data.weight,
-            type:response.data.types[0].type.name,
-            hp:response.data.stats[0].base_stat,
-            attack:response.data.stats[1].base_stat,
-            speed:response.data.stats[5].base_stat,
-            defense:response.data.stats[2].base_stat,
-            specialAttack:response.data.stats[3].base_stat,
-            specialDefense:response.data.stats[4].base_stat
+const Item = ({ name, url }) => {
+  const [itemData, setItemData] = useState({
+    name,
+    imageUrl: "",
+    ability: "",
+    height: 0,
+    weight: 0,
+    type: "",
+    hp: 0,
+    attack: 0,
+    speed: 0,
+    defense: 0,
+    specialAttack: 0,
+    specialDefense: 0,
+  });
 
-          }));
-        };
-    
-        fetchData();
-      }, [url]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url);
+      setItemData((prev) => ({
+        ...prev,
+        imageUrl: response.data.sprites.front_default,
+        ability: response.data.abilities[0].ability.name,
+        height: response.data.height,
+        weight: response.data.weight,
+        type: response.data.types[0].type.name,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        speed: response.data.stats[5].base_stat,
+        defense: response.data.stats[2].base_stat,
+        specialAttack: response.data.stats[3].base_stat,
+        specialDefense: response.data.stats[4].base_stat,
+      }));
+    };
 
-      return(
-        <div className="pokemon">
-            <div className="header">{name}</div>
-            <div className="subcontainer">
-            <img className="image" src={itemData.imageUrl}/>
-            <div className="info">
-            
-            <div className="ability-key">Ability:</div><span className="ability-value">{itemData.ability}</span>
-            <div className="ability-key">Height:</div><span className="ability-value">{itemData.height/10} m</span>
-            <div className="ability-key">Weight:</div><span className="ability-value">{itemData.weight/10} kg</span>
-            <div className="ability-key">Type:</div><span className="ability-value">{itemData.type}</span>
-            </div>
-            </div>
-            <div className="stat">
-              <div className="stat-header">Stat</div>
-              <div className="stat-bar">
-              <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"green",
-                textColor:"green",
-                textSize:"12px"
-              })} value={itemData.hp} text={`${itemData.hp} hp`} />
-                </div>
-                <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"goldenrod",
-                textColor:"goldenrod",
-                textSize:"12px"
-              })} value={itemData.speed} text={`${itemData.speed} Speed`} />
-                </div>
-                <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"orangered",
-                textColor:"orangered",
-                textSize:"12px"
-              })} value={itemData.attack} text={`${itemData.attack} Attack`} />
-                </div>
-                <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"darkblue",
-                textColor:"darkblue",
-                textSize:"12px"
-              })} value={itemData.defense} text={`${itemData.defense} Defense`} />
-                </div>
-                <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"mediumorchid",
-                textColor:"mediumorchid",
-                textSize:"12px"
-              })} value={itemData.specialAttack} text={`${itemData.specialAttack} S.Attack`} />
-                </div>
-                <div style={{ width: 70, height: 70}}>
-              <CircularProgressbar styles ={buildStyles({
-                pathColor:"dodgerblue",
-                textColor:"dodgerblue",
-                textSize:"12px"
-              })} value={itemData.specialDefense} text={`${itemData.specialDefense} S.Defense`} />
-                </div>
-            </div>
-            </div>
+    fetchData();
+  }, [url]);
+
+  return (
+    <div className="pokemon">
+      <div className="header">{name}</div>
+      <div className="subcontainer">
+        <img className="image" src={itemData.imageUrl} />
+        <div className="info">
+          <div className="info-item">
+            <span className="ability-key">Ability:</span>
+            <span className="ability-value">{itemData.ability}</span>
+          </div>
+          <div className="info-item">
+            <span className="ability-key">Height:</span>
+            <span className="ability-value">{itemData.height / 10} m</span>
+          </div>
+          <div className="info-item">
+            <span className="ability-key">Weight:</span>
+
+            <span className="ability-value">{itemData.weight / 10} kg</span>
+          </div>
+          <div className="info-item">
+            <span className="ability-key">Type:</span>
+            <span className="ability-value">{itemData.type}</span>
+          </div>
         </div>
-      )
-}
+      </div>
+      <div className="stat">
+        <div className="stat-header">Stat</div>
+        <div className="stat-bar">
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "green",
+                textColor: "green",
+                textSize: "12px",
+              })}
+              value={itemData.hp}
+              text={`${itemData.hp} hp`}
+            />
+          </div>
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "goldenrod",
+                textColor: "goldenrod",
+                textSize: "12px",
+              })}
+              value={itemData.speed}
+              text={`${itemData.speed} Speed`}
+            />
+          </div>
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "orangered",
+                textColor: "orangered",
+                textSize: "12px",
+              })}
+              value={itemData.attack}
+              text={`${itemData.attack} Attack`}
+            />
+          </div>
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "darkblue",
+                textColor: "darkblue",
+                textSize: "12px",
+              })}
+              value={itemData.defense}
+              text={`${itemData.defense} Defense`}
+            />
+          </div>
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "mediumorchid",
+                textColor: "mediumorchid",
+                textSize: "12px",
+              })}
+              value={itemData.specialAttack}
+              text={`${itemData.specialAttack} S.Attack`}
+            />
+          </div>
+          <div style={{ width: 70, height: 70 }}>
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: "dodgerblue",
+                textColor: "dodgerblue",
+                textSize: "12px",
+              })}
+              value={itemData.specialDefense}
+              text={`${itemData.specialDefense} S.Defense`}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
